c869caa4070dfa8e3ea461de5543827a2012a8d2
Kristin Gumbert <kristingumbert@Kristins-MacBook-Pro.local>
2016-07-06 11:57:54 -0400

    adding needed files

---
+++ keys.js
@@ -0,0 +1,8 @@
+console.log('this is loaded');
+
+exports.twitterKeys = {
+  consumer_key: ' iUsBPwoGKoT96l00Z3Aj5PQdq',
+  consumer_secret: ' 10qUukAUij5WxAfakh4CVt4UlbaYlBPdAvvllIsknzsPS5Flwz',
+  access_token_key: ' 33325446-GqJ5zDeWj5u4Ytwv3x66Kj5yiSSPsZDf8ju5yl74p',
+  access_token_secret: 'TIpvukAAh8XtRXzFPWYoUzXkiRb6hvXABxDvQHaZdmXZU',
+}
